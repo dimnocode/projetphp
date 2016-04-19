dd27f06ac366488821361254695f12603c1ea5b0
jmabe <jm.abe@labruyere.be>
2016-04-19 19:54:11 +0200

    ajout core

---
+++ models/model.php
@@ -0,0 +1,103 @@
+﻿<?php
+class Model{
+	Public $table;
+	private $connection;
+	
+	function __construct() {
+		
+		try {
+
+		  $dns = 'mysql:host=127.0.0.1;dbname=northwind';
+		  $utilisateur = "northwind";
+		  $motDePasse = 'northwind';
+		 
+		  // Options de connection
+		  $options = array(
+			PDO::MYSQL_ATTR_INIT_COMMAND    => "SET NAMES utf8"
+		  );
+		 
+		  // Initialisation de la connection
+		  $this->connection = new PDO( $dns, $utilisateur, $motDePasse, $options );
+		} catch ( Exception $e ) {
+		  echo "Connection à MySQL impossible : ", $e->getMessage();
+		  die();
+		}
+	}
+	
+	public function read($fields=null,$search=null){
+		if($fields==null){
+			$fields = '*';
+		}
+		if ($search== null){
+			$sql= 'SELECT '.$fields.' from '.$this->table ;
+		}
+		else{
+         $sql='';
+         foreach($search as $key => $val) {
+            if (isset($key)) {
+            if ($sql!='') {
+               $sql .=' and ';
+            }
+            $sql .=$key." like '%".$val."%'";
+         }
+         }
+			$sql= 'SELECT '.$fields.' from '.$this->table .'  where '.$sql;
+		}
+      
+		
+		
+		try {
+		  // On envois la requète
+		  $select = $this->connection->query($sql);
+		  // On indique que nous utiliserons les résultats en tant qu'objet
+		$select->setFetchMode(PDO::FETCH_OBJ);
+		$this->data = new stdClass();
+			$this->data = $select->fetchAll();
+
+		} catch ( Exception $e ) {
+		  echo 'Une erreur est survenue lors de la récupération des créateurs';
+		}
+	}
+	
+	public function rtv_Table(){
+	
+			
+			$out  = "";
+			
+			$titre= '<tr>';
+			$titre_trt= false;
+			
+			foreach($this->data as $key => $element){
+				$out .= "<tr>";
+				foreach($element as $subkey => $subelement){
+					if($titre_trt==false){
+						$titre .= '<th>'.$subkey.'</th>' ;	
+					}
+					
+					$out .= '<td>'.$subelement.'</td>' ;
+				}
+				if($titre_trt==false){
+					$titre.= '</tr>';
+				}
+				$titre_trt= true;
+				$out .= "</tr>";
+			}
+			$out = '<table>'.$titre.$out.'</table>';
+			
+
+			return $out;
+
+	}
+	
+	static function load($name){
+		require ($name.'.php');
+		return new $name();
+	}
+	
+
+}
+
+
+
+?>
+
